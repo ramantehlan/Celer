53dca9c203168052dc8bc5fdddb2ed2b55181042
Raman Tehlan <ramantehlan@gmail.com>
2017-09-09 02:39:44 +0530

    Minor changes

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/comman-ui.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/index-ui.css">
 
+
 	<script type="text/javascript" src='assets/javascript/jquery.js'></script>
 	<script type="text/javascript" src='assets/javascript/jquery-ui.js'></script>
+	<script type="text/javascript" src='assets/javascript/index.js'></script>
 </head>
 <body>
+
+	<div class=''>
+
 
 </body>
 </html>
