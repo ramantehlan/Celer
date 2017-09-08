374c904f890db23a0143b78b84a15f9331c2d3c1
Raman Tehlan <ramantehlan@gmail.com>
2017-09-09 01:38:13 +0530

    Initial Create

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Celer - #1 Processing distribution system</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/comman-ui.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/index-ui.css">
+
+	<script type="text/javascript" src='assets/javascript/jquery.js'></script>
+	<script type="text/javascript" src='assets/javascript/jquery-ui.js'></script>
+</head>
+<body>
+
+</body>
+</html>
