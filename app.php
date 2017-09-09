40d55f3ec956a83e512c73a84a551b883cd2aeff
Raman Tehlan <ramantehlan@gmail.com>
2017-09-09 06:36:54 +0530

    New File

---
+++ app.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Celer</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/comman-ui.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/leftmenu-ui.css">
+
+
+	<script type="text/javascript" src='assets/javascript/jquery.js'></script>
+	<script type="text/javascript" src='assets/javascript/jquery-ui.js'></script>
+</head>
+<body>
+
+<div class="left_menu">
+
+</div>
+
+</body>
+</html>
