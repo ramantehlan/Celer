969f9d59a715ac8a5881581849959604ae2c8fcf
Raman Tehlan <ramantehlan@gmail.com>
2017-09-09 22:22:22 +0530

    Minor changes

---
+++ b_upload.php
@@ -0,0 +1,11 @@
+<div class='form'>
+		<div class='form_heading'>
+		Upload Program
+		</div>
+		<div class='form_body'>
+				<input type='file' class='input' id='file'>
+
+				<input type='submit' class='submit'>
+		</div>
+</div>
+
