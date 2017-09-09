a3a6d592672534a3cb6ab22d5025b8ad2dcc8ef9
Raman Tehlan <ramantehlan@gmail.com>
2017-09-09 12:42:48 +0530

    Minor changes

--- a_dashboard.php
+++ a_dashboard.php
@@ -1,1 +1,29 @@
-a_dashboard
+<div class='group_0'>
+
+       
+               <div class='cards box_body effect_slide_right_1'>
+                        <div class="box_body_top">
+                                <div class="box_heading small_head heading_total">
+                                    <img src="assets/images/users.png" > 
+                                            Total users
+                                </div>
+                        </div>
+                        <div class='box_content '  >
+                                
+                        </div>
+               </div>
+
+               <div class='cards box_body effect_slide_right_2'>
+                        <div class="box_body_top">
+                                <div class="box_heading small_head heading_active">
+                                    <img src="assets/images/wifi.png" >
+                                        Active users
+                                </div>
+                        </div>
+                        <div class='box_content' >
+                                
+                            
+                        </div>
+               </div>
+    
+        </div>
