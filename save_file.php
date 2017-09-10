ffd31642825c2ba75668153e31de832c179c5730
Raman Tehlan <ramantehlan@gmail.com>
2017-09-10 05:49:54 +0530

    minor changes

---
+++ save_file.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	if(isset($_FILES['program'])){
+		if($_FILES['program']['tmp_name']){
+			if(move_uploaded_file($_FILES['program']['tmp_name'], "uploads/" . "sample.java" )){
+     											
+     									echo "done";
+     		}else{
+     			  echo "File not moved";
+     		}
+		}else{
+			echo "Temp file not present";
+		}
+	}else{
+		echo "File not sent";
+	}
+
+?>
